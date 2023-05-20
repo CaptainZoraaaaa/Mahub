5f06efb3eb11307197468f6579be3d8aa7153839
CaptainZoraaaaa <max.svedala@gmail.com>
2023-05-20 11:56:22 +0200

    börjat testa websockets i javascripten

---
+++ Webbpage/Javascript/Register.js
@@ -0,0 +1,7 @@
+let socket = new WebSocket("wss://javascript.info/article/websocket/demo/hello");
+
+socket.onopen = function(e) {
+  alert("[open] Connection established");
+  alert("Sending to server");
+  socket.send("My name is John");
+};
