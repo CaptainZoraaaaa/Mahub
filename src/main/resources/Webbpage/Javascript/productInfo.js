72064e855a35accff7fa0136c981ca818254990a
CaptainZoraaaaa <max.svedala@gmail.com>
2023-05-21 17:25:46 +0200

    update

--- src/main/resources/Webbpage/Javascript/productInfo.js
+++ src/main/resources/Webbpage/Javascript/productInfo.js
@@ -1,15 +1,19 @@
 var urlParam = new URLSearchParams(location.search);
+var id;
+var dataObject;
+
+
 
 
 $(document).ready(function () {
-    var id = urlParam.get("id");
+    id = urlParam.get("id");
     console.log(id);
     $.ajax({
       method: "GET",
       url: "http://localHost:5500/getProduct/" + id,
       headers: {"Accept": "application/Json"}
     }).done(function (data){
-        console.log(data);
+        dataObject = data;
         document.querySelector("#image_area").style.backgroundImage = "url("+data.image+")";
         $('#file_chooser').append(data.productName);
         $('#productName').append(data.productName);
@@ -17,4 +21,17 @@ $(document).ready(function () {
         $('#price').append(data.price);
         // ========================================================================================
     })
-  });
+});
+
+function addToCart(){
+    var jsonArray = localStorage.getItem("jsonArray");
+    var array = JSON.parse(jsonArray);
+    array[array.length] = dataObject;
+
+    jsonArray = JSON.stringify(array);
+    localStorage.setItem('jsonArray', jsonArray);
+  
+    array.forEach(element => {
+        console.log(element.productId);
+    });
+}
