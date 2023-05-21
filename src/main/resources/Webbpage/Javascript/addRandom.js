99b430ceabdf66bc6f0101e7fbb00d693d0f9734
CaptainZoraaaaa <max.svedala@gmail.com>
2023-05-21 13:48:54 +0200

    Kan nu hämta produkter

--- src/main/resources/Webbpage/Javascript/addRandom.js
+++ src/main/resources/Webbpage/Javascript/addRandom.js
@@ -1,9 +1,17 @@
 
 var offset = 0;
+var productIds = [];
+
 $(document).ready(function () {
     getStart();
 
 });
+
+function showDetails(index){
+  console.log(recipeIds[index]);
+  window.location.href = 'prductInfo.html' + '?id=' + productIds[index]; 
+} 
+
 function getStart(){
   $.ajax({
       method: "GET",
@@ -12,7 +20,8 @@ function getStart(){
   }).done(function (data){
     console.log(data);
     for(i=offset; i<data.length+offset; i++){
-      $('#randomProduct').append('<div class="flex-item" id="d'+i+'" > <img src="'+data[i].image+'" alt="Image" id="img'+data.s+'"> <p id="p'+i+'">'+data[i].productName+'</p></div>');
+      $('#randomProduct').append('<div class="flex-item" id="d'+i+'" > <img src="'+data[i].image+'" alt="Image" id="img'+data.s+'"> <p id="p'+i+'">'+data[i].productName+'</p> <button type="button" onclick="showDetails('+i+')">Show details</button></div>');
+      productIds[i] = data[i].id;
       offset++;
     }
   })
