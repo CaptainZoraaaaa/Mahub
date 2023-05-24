f05753c17a3299307a0f597c40d45c6e860fba76
William817817 <williamdock99@gmail.com>
2023-05-24 21:28:24 +0200

    new new

--- src/main/resources/Webbpage/Javascript/Checkout.js
+++ src/main/resources/Webbpage/Javascript/Checkout.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     const jsonArray = localStorage.getItem("jsonArray");
     const array = JSON.parse(jsonArray);
 
+    var cartContainer = document.getElementById('cart-items');
+
+    console.log(array);
+
     array.forEach(function(item) {
       var cartItem = document.createElement('div');
       cartItem.className = 'cart-item';
@@ -10,9 +14,18 @@ $(document).ready(function () {
       image.src = item.image;
       cartItem.appendChild(image);
       
-      var itemName = document.createElement('span');
-      itemName.textContent = item.name;
+      var itemName = document.createElement('div');
+      itemName.className = 'item-name';
+      itemName.textContent = item.productName;
       cartItem.appendChild(itemName);
+<<<<<<< Updated upstream
+=======
+
+      var cost = document.createElement('div');
+      cost.className = 'cost-class';
+      cost.textContent = item.price;
+      cartItem.appendChild(cost);
+>>>>>>> Stashed changes
       
       cartContainer.appendChild(cartItem);
     });
